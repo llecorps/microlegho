d30135cb4ca121928a3c6045e5e6088cb9906fcf
esspressoh <laurent.lecorps@gmail.com>
2022-09-10 08:19:44 +0200

    End task loggin form

--- src/main/java/ch/rts/si/microservice/microlegho/controller/LeghoController.java
+++ src/main/java/ch/rts/si/microservice/microlegho/controller/LeghoController.java
@@ -2,10 +2,12 @@ package ch.rts.si.microservice.microlegho.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 public class LeghoController {
     @GetMapping("/")
